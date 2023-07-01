6cb909b2d1dd45ad4961bc2453a4782d7a38803b
Fermin Blanco <luillyfe89@gmail.com>
2023-07-01 18:08:42 -0500

    feature: continue scrolling behaviour

--- src/app/home/components/Footer.tsx
+++ src/app/home/components/Footer.tsx
@@ -1,8 +1,16 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { getNextPage } from "@/app/actions";
+import usePageBottom from "@/hooks/usePageBottom";
 
+// TODO: If images are still loading, disable scroll handler
 export default function Footer() {
-  return <div onClick={() => getNextPage()}>Footer</div>;
+  const isAtPageBottom = usePageBottom();
+
+  useEffect(() => {
+    if (isAtPageBottom) getNextPage();
+  }, [isAtPageBottom]);
+
+  return <div>Footer</div>;
 }
